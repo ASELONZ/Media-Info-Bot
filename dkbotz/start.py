eb33a1c0faeca158821c11062edf010d32cccd86
ASELONZ <93417104+ASELONZ@users.noreply.github.com>
2022-05-18 18:57:05 +0530

    Update start.py

--- dkbotz/start.py
+++ dkbotz/start.py
@@ -13,7 +13,7 @@ async def start(bot: Client, cmd: Message):
     await cmd.reply_text(
         f"""**Hello {cmd.from_user.mention}**
 
-**I Am Most Powerful Media Info Bot.**
+**I Am Most Powerful Media Info Bot Created By The ꉓ𝕺ค.**
 
 **Use Me To Generate Infomation Of Media Like Photo, Videos And Document.**
 
@@ -22,10 +22,11 @@ async def start(bot: Client, cmd: Message):
             [[
             InlineKeyboardButton('➕ Add Me To Your Group ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('📢 Bot Channel', url='http://t.me/DKBOTZ'),
-            InlineKeyboardButton('💼 Support Group', url=f'http://t.me/{Config.SUPPORT_GROUP}')
+            InlineKeyboardButton('𝐃𝐄𝐅𝐄𝐍𝐃𝐄𝐑 𝐎𝐅 𝐓𝐇𝐄 𝐌𝐔𝐋𝐓𝐈𝐕𝐄𝐑𝐒𝐄 ', url='http://t.me/defenderofthemultiverse'),
+            InlineKeyboardButton('𝓽ꫝꫀ ᥴ𝘳ꫀꪖ𝓽ꪮ𝘳 ꪮᠻ ꪖꪶꪶ', url=f'http://t.me/{Config.SUPPORT_GROUP}')
             ],[
-            InlineKeyboardButton('🌐 Source Code 🌐', url='https://github.com/DKBOTZHELP/Media-Info-Bot')
+            InlineKeyboardButton('ꉓ𝕺ค', url='https://t.me/ANKIT3690'),
+            InlinekeyboardButton('꧁༒༺☠︎☬ S҉₳𝖀𝕽₳V҉☠︎☬༒꧂', url='https://t.me/Saurav3BV6SA9LLElon7Musk')
             ],[
             InlineKeyboardButton('⌦ Close The Menu ⌫', callback_data='close_data')
         ]]
