3964b26c61164b84fcdc75a8ac988d4268b47ec0
ASELONZ <93417104+ASELONZ@users.noreply.github.com>
2022-11-03 15:17:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/DKBOTZHELP/Media-Info-Bot /Media-Info-Bot
+  git clone https://github.com/ASELONZ/Media-Info-Bot /Media-Info-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/DKBOTZHELP/Media-Info-Bot -b $BRANCH /Media-Info-Bot
+  git clone https://github.com/ASELONZ/Media-Info-Bot -b $BRANCH /Media-Info-Bot
 fi
 cd /Media-Info-Bot
 pip3 install -U -r requirements.txt
